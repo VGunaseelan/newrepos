2620e52874c253f6ad101876b92aeb9c771e6721
VGunaseelan <51106746+VGunaseelan@users.noreply.github.com>
2021-04-12 17:49:42 +0530

    fetchcard

---
+++ fetch.php
@@ -0,0 +1,16 @@
+<?php
+
+$jsonobject = '{    
+    "primaryAction": {
+      "type": "IFRAME",
+      "width": 890,
+      "height": 748,
+      "uri": "http://localhost/hubspot/hubspot_sms/template.php?phone=7200787807&token=d49c7d8dfc80d6e7d7a11c9959ed365c&name=Gunaseelan",
+      "label": "Send SMS with MOBtexting"
+    }
+    
+}';
+
+$cardData = json_decode($jsonobject);
+
+?>
